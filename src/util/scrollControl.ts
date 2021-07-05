6dad3d48409dd3905b7aaaac4b060898c06f9606
dbushniev <dbushniev@qualium-systems.com.ua>
2021-07-05 16:43:05 +0300

    Change list scroll buttons

--- src/util/scrollControl.ts
+++ src/util/scrollControl.ts
@@ -1,18 +1,19 @@
 enum Keys {
-  ArrowDown = 'ArrowDown',
-  ArrowUp = 'ArrowUp'
+  PageUp = 'PageUp',
+  PageDown = 'PageDown'
 }
 
 const KEYS: {[key: string]: string} = {
-  [Keys.ArrowDown]: 'ArrowDown',
-  [Keys.ArrowUp]: 'ArrowUp',
+  [Keys.PageDown]: 'PageDown',
+  [Keys.PageUp]: 'PageUp',
 }
 
 const scrollControl = (e: KeyboardEvent) => {
+  console.log(e.key)
   const list = document.querySelector<HTMLDivElement>('.fsp-content-scroll');
   if (list && list.scrollHeight > list.clientHeight && KEYS[e.key]) {
     e.preventDefault();
-    if (KEYS[e.key] === KEYS[Keys.ArrowDown]) return list.scrollTop += 100;
+    if (KEYS[e.key] === KEYS[Keys.PageDown]) return list.scrollTop += 100;
     return list.scrollTop += -100
   }
 }
